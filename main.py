825ab1d27a71358fa428569bab58f337a57c7342
Niklas Dahlstrand <nikgu@student.chalmers.se>
2018-02-19 17:14:51 +0100

    Commit 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Imports
+import math as m
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
@@ -28,13 +29,17 @@ np.set_printoptions(precision=4)
 
 
 def read_coordinate_file(filename):
+    """
+    :param filename: Textfile with coordinates given in latitude and longitude
+    :return: np.array with x and y coordinates
+    """
     file = open(filename, "r")
     x = []
     y = []
     for line in file:
         lista = line.rstrip().strip("{").strip("}")
         coord_ab = lista.split(",")
-        x.append(float(coord_ab[1]) * np.pi / 180)
+        x.append(float(coord_ab[1]) * np.pi / 180)  # x and y coordinates calculated using Mercator projection
         y.append(float(np.log(np.tan((np.pi / 4) + (np.pi * float(coord_ab[0])) / 360))))
     coord_xy = np.array([x, y])
     file.close()
@@ -47,11 +52,11 @@ def plot_points2(coord_list, path):
     lines = []
     line = []
     for i in range(len(path) - 1):
-        x0 = coord_list[0, int(path[i])]
-        y0 = coord_list[1, int(path[i])]
+        x0 = coord_list[0, path[i]]
+        y0 = coord_list[1, path[i]]
         line.append((x0, y0))
-        x1 = coord_list[0, int(path[i + 1])]
-        y1 = coord_list[1, int(path[i + 1])]
+        x1 = coord_list[0, path[i + 1]]
+        y1 = coord_list[1, path[i + 1]]
         line.append((x1, y1))
         lines.append(line)
         line = []
@@ -68,13 +73,12 @@ def plot_points(coord_list, connections, path):
     line = []
     a = np.array((connections[0, :]))
     b = np.array((connections[1, :]))
-    soize = np.size(a)
-    for i in np.arange(soize):
-        x0 = coord_list[0, int(a[i])]
-        y0 = coord_list[1, int(a[i])]
+    for i in np.arange(a.size):
+        x0 = coord_list[0, a[i]]
+        y0 = coord_list[1, a[i]]
         line.append((x0, y0))
-        x1 = coord_list[0, int(b[i])]
-        y1 = coord_list[1, int(b[i])]
+        x1 = coord_list[0, b[i]]
+        y1 = coord_list[1, b[i]]
         line.append((x1, y1))
         lines.append(line)
         line = []
@@ -87,7 +91,7 @@ def plot_points(coord_list, connections, path):
 
 # Uppgift 3.
 def distance(x0, x1, y0, y1):
-    dist = np.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+    dist = m.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
     return dist
 
 
@@ -96,11 +100,12 @@ def construct_graph_connections(coord_list, radius):
     y_coord = coord_list[1, :]
     points = []
     real_dist = []
-    for i in range(len(x_coord)):
-        for z in range(1 + i, len(x_coord)):
-            dist = distance(x_coord[i], x_coord[z], y_coord[i], y_coord[z])
-            if dist < radius:
-                points.append([i, z])
+
+    for i, j in enumerate(x_coord):
+        for k, l in enumerate(x_coord):
+            dist = distance(j, l, y_coord[i], y_coord[k])
+            if dist < radius and dist != 0:
+                points.append([i, k])
                 real_dist.append(dist)
     points = np.array(points)
     points = points.transpose()
@@ -110,26 +115,34 @@ def construct_graph_connections(coord_list, radius):
 # Uppgift 4.
 
 
-def construct_graph(indices, distances):
-    N = int(np.max(indices[1, :]) + 1)
+def construct_graph(indices, distances, N):
     matrix = csr_matrix((distances, (indices[0, :], indices[1, :])), shape=(N, N))
     return matrix
 
 
-def shortest_path(graph, start, end, direction):
+def shortest_path(graph, start, end):
     """
     :param graph: a csr graph
     :param start: start node
     :param end: end node
-    :param direction: if the graph is directed or not
     :return: shortest distance and a list of predecessors
+
+    shortest is a list which describes the distance to all cities from the start node. Noted with the index.
+    ex: distance from start to node 10 = shortest[10].
+
+    pre_all is a predecessor list that describes which city is closest to current city.
+    ex: if pre_all = [-9999 2 0 4 0 3 4], then the closest city to city 5 is pre_all[5], which is city 3.
+    The closest to city 3 is then pre_all[3] = 4 and so on. -9999 means that no path exists.
+
     """
     pre_list = []
-    shortest, pre_all = dijkstra(graph, directed=direction, indices=[start, end], return_predecessors=True)
+    # indices indicates which nodes to start from
+    # return_predecessors indicates that dijkstra returns a predecessor list as well
+    shortest, pre_all = dijkstra(graph, indices=start, return_predecessors=True)
     i = end
     pre_list.append(end)
-    while pre_all[0, i] != start:
-        i = pre_all[0, i]
+    while pre_all[i] != start:  # Loop to get the predecessor list from node start to end
+        i = pre_all[i]
         pre_list.append(i)
     pre_list.append(start)
     pre_list.reverse()
@@ -137,26 +150,32 @@ def shortest_path(graph, start, end, direction):
 
 
 def construct_fast_graph_connections(coord_list, radius):
-    points = np.c_[coord[0, :], coord_list[1, :]]
+    """
+    :param coord_list: 2xn array of coordinates x and y for n points
+    :param radius: maximum radius between reachable cities
+    :return:
+    points: a 2xm np.array of city combinations within the given radius. m number of combinations
+        ex: points = [[0 1 ...]
+                      [1 2 ...]] where city 1 can be reached from city 0 and 2 from 1.
+    real_dist: 1xm np.array of distances between the reachable cities.
+        ex: real_dist = [0.5 1 0.3...] where the distance between 0 and 1 is 0.5
+    """
+    points = coord_list.transpose()
     tree = cKDTree(points)
     x_coord = coord_list[0, :]
     y_coord = coord_list[1, :]
-    t = time.time()
-    stuff = tree.query_ball_point(points, radius)
+    reach = tree.query_ball_point(points, radius)   # Creates a list of lists where each list represent which cities can
+                                                    # be reached from city index in reach
     points = []
     real_dist = []
-    t = time.time()
-    test = []
-    for i in range(len(stuff)):
+    for i in range(len(reach)):  # Loop through all lists in reach, to determine distances between each accessible city
         x1 = x_coord[i]
         y1 = y_coord[i]
-        test.append(i)
-        #l3 = [x for x in stuff[i] if x not in test]  # Used to shorten points, makes it non-directed
-        for j in stuff[i]:
+        for j in reach[i]:
             if j != i:
                 x2 = x_coord[j]
                 y2 = y_coord[j]
-                dist = ma.sqrt((x2-x1)**2 + (y2-y1)**2)
+                dist = distance(x1, x2, y1, y2)
                 points.append([i, j])
                 real_dist.append(dist)
     points = np.array(points)
@@ -167,11 +186,11 @@ def construct_fast_graph_connections(coord_list, radius):
 
 t_total = time.time()
 # User interface, choose city and fast or slow construct_graph
-name = "GermanyCities.txt"
-radius = GERMANY_RADIUS
-start = GERMANY_START
-end = GERMANY_END
-fast_graph = True  # True if fast, False if slow
+name = "SampleCoordinates.txt"
+radius = SAMPLE_RADIUS
+start = SAMPLE_START
+end = SAMPLE_END
+which_graph = True  # True if fast, False if slow
 
 
 t = time.time()
@@ -180,21 +199,22 @@ print("The time it takes to read_coordinate_file: ", time.time() - t)
 
 
 t = time.time()
-if fast_graph:
+if which_graph:
     points, dist = construct_fast_graph_connections(coord, radius)
-    directed = True
 else:
     points, dist = construct_graph_connections(coord, radius)
-    directed = False
 print("The time it takes to construct_fast_graph_connections: ", time.time() - t)
 
+N = coord[0, :].size   # Number of unique cities
+
+
 t = time.time()
-csr = construct_graph(points, dist)
+csr = construct_graph(points, dist, N)
 print("The time it takes to construct_graph: ", time.time() - t)
 
 
 t = time.time()
-dist_matrix, predecesor = shortest_path(csr, start, end, directed)
+dist_matrix, predecesor = shortest_path(csr, start, end)
 print("The time it takes to calculate 6 and 7: ", time.time() - t)
 
 print("The total time excluding the plot task: ", time.time() - t_total)
@@ -203,7 +223,7 @@ t = time.time()
 plot_points(coord, points, predecesor)
 print("The time it takes to plot_points: ", time.time() - t)
 
-print("The shortest distance is: ", dist_matrix[0, end])
+print("The shortest distance is: ", dist_matrix[end])
 print("The shortest path is: ", predecesor)
 
 
@@ -213,7 +233,7 @@ plt.show()
 if name == "SampleCoordinates.txt":
     file_result = open("SampleCoordinatesResult.txt", "w")
     file_result.write("The shortest distance:\n")
-    file_result.write(str(dist_matrix[0, end]) + "\n")
+    file_result.write(str(dist_matrix[end]) + "\n")
     file_result.write("The cities: \n")
     for line in predecesor:
         file_result.write(str(line) + "\n")
@@ -222,7 +242,7 @@ if name == "SampleCoordinates.txt":
 elif name == "HungaryCities.txt":
     file_result = open("HungaryCitiesResult.txt", "w")
     file_result.write("The shortest distance:\n")
-    file_result.write(str(dist_matrix[0, end])+"\n")
+    file_result.write(str(dist_matrix[end])+"\n")
     file_result.write("The cities: \n")
     for line in predecesor:
         file_result.write(str(line)+"\n")
@@ -231,7 +251,7 @@ elif name == "HungaryCities.txt":
 elif name == "GermanyCities.txt":
     file_result = open("GermanyCitiesResult.txt", "w")
     file_result.write("The shortest distance:\n")
-    file_result.write(str(dist_matrix[0, end]) + "\n")
+    file_result.write(str(dist_matrix[end]) + "\n")
     file_result.write("The cities: \n")
     for line in predecesor:
         file_result.write(str(line) + "\n")
